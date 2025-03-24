206827ddfef2ab35716d59334e8b4570764e363e
mufaddals <mufaddal.saifee52@gmail.com>
2025-03-24 15:53:25 +0530

    # changes

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-12">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("gray")}
+            style={{ backgroundColor: "gray" }}
+          >
+            Gray
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("red")}
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("green")}
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("orange")}
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
